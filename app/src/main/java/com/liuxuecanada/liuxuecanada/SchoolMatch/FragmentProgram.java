8239412044b3208623632139e348a5e17dcf77b2
superman566 <ghe@lakeheadu.ca>
2015-09-26 00:11:23 -0400

    overwrite functions in fragment files

--- app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/FragmentProgram.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/FragmentProgram.java
@@ -13,6 +13,7 @@ public class FragmentProgram extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_studentchoicesprogram, container, false);
+        View v = inflater.inflate(R.layout.fragment_studentchoicesprogram,container,false);
+        return v;
     }
 }
