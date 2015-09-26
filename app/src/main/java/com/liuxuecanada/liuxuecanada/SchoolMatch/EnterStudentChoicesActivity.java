5e8e54486f9ef172d7371d8ed2c4591e16d20cc2
Licong <licongdavis@hotmail.com>
2015-09-25 21:38:31 -0400

    Update fragments

--- app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
@@ -1,16 +1,58 @@
 package com.liuxuecanada.liuxuecanada.SchoolMatch;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 import com.liuxuecanada.liuxuecanada.R;
 
-public class EnterStudentChoicesActivity extends Activity {
+public class EnterStudentChoicesActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_studentchoicesprogram);
+        setContentView(R.layout.fragment_studentchoicesmain);
 
+        FragmentProgram firstFragment = new FragmentProgram();
+        setFragmentView("program");
 
+    }
+
+    private void setFragmentView(String whichFragment) {
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+/*            if (savedInstanceState != null) {
+                return;
+            }*/
+
+            if (whichFragment == "program") {
+                FragmentProgram firstFragment = new FragmentProgram();
+                // In case this activity was started with special instructions from an
+                // Intent, pass the Intent's extras to the fragment as arguments
+                firstFragment.setArguments(getIntent().getExtras());
+                // Add the fragment to the 'fragment_container' FrameLayout
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+            } else if (whichFragment == "languagetest") {
+                FragmentLanguagetest firstFragment = new FragmentLanguagetest();
+                // In case this activity was started with special instructions from an
+                // Intent, pass the Intent's extras to the fragment as arguments
+                firstFragment.setArguments(getIntent().getExtras());
+                // Add the fragment to the 'fragment_container' FrameLayout
+                FragmentTransaction transactionLanguagetest = getSupportFragmentManager().beginTransaction();
+                transactionLanguagetest.replace(R.id.fragment_container, firstFragment);
+                transactionLanguagetest.addToBackStack(null);
+                transactionLanguagetest.commit();
+
+            }
+        }
+    }
+
+    public void clickStudentProgramButton(View view) {
+        setFragmentView("languagetest");
     }
 }
---
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/FragmentLanguagetest.java
@@ -0,0 +1,18 @@
+package com.liuxuecanada.liuxuecanada.SchoolMatch;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.liuxuecanada.liuxuecanada.R;
+
+public class FragmentLanguagetest extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_studentchoiceslanguagetest, container, false);
+    }
+}
