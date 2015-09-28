4c8b58785f98d2ba359560dce94e56b1792a2c7e
Licong Tang <litang@blackberry.com>
2015-09-28 17:05:24 -0400

    Update layout

--- app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/FragmentGPA.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/FragmentGPA.java
@@ -1,10 +1,12 @@
 package com.liuxuecanada.liuxuecanada.SchoolMatch;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 
 import com.liuxuecanada.liuxuecanada.R;
 
@@ -12,6 +14,16 @@ public class FragmentGPA extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_studentchoicesgpa, container, false);
+        View v = inflater.inflate(R.layout.fragment_studentchoicesgpa, container, false);
+        startAuto();
+        return v;
+    }
+
+    private void
+    startAuto(){
+        ObjectAnimator animation = ObjectAnimator.ofInt ( R.layout.fragment_studentchoicesgpa, "progress", 1, 100);
+        animation.setDuration (5000); //in milliseconds
+        animation.setInterpolator (new DecelerateInterpolator());
+        animation.start ();
     }
 }
