d26835ec93406e0341e16d1bedb6e89b7563dd37
Licong <licongdavis@hotmail.com>
2015-09-28 20:50:49 -0400

    Update proceed logic

--- app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
@@ -3,6 +3,7 @@ package com.liuxuecanada.liuxuecanada.SchoolMatch;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,6 +14,8 @@ import com.liuxuecanada.liuxuecanada.R;
 public class EnterStudentChoicesActivity extends FragmentActivity {
 
     private Fragment frag = null;
+    private String currentFragment = null;
+    private String nextFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +40,26 @@ public class EnterStudentChoicesActivity extends FragmentActivity {
 
             if (whichFragment == "program") {
                 frag = new FragmentProgram();
-            } else if (whichFragment == "languagetest") {
-                frag = new FragmentLanguageTest();
-            } else if (whichFragment == "toefl") {
-                frag = new FragmentTOEFL();
-            } else if (whichFragment == "ielts") {
-                frag = new FragmentIELTS();
-            } else if (whichFragment == "gpa") {
-                frag = new FragmentGPA();
-            } else if (whichFragment == "gpacalculator") {
-                frag = new FragmentGPACalculator();
+                frag.setArguments(getIntent().getExtras());
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, frag).commit();
+                setCurrentFragment(whichFragment);
+            } else {
+                if (whichFragment == "languagetest") {
+                    frag = new FragmentLanguageTest();
+                } else if (whichFragment == "toefl") {
+                    frag = new FragmentTOEFL();
+                } else if (whichFragment == "ielts") {
+                    frag = new FragmentIELTS();
+                } else if (whichFragment == "gpa") {
+                    frag = new FragmentGPA();
+                } else if (whichFragment == "gpacalculator") {
+                    frag = new FragmentGPACalculator();
+                }
+                frag.setArguments(getIntent().getExtras());
+                //getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, frag).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, frag).addToBackStack(null).commit();
+                setCurrentFragment(whichFragment);
             }
-
-            frag.setArguments(getIntent().getExtras());
-            //getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, frag).commit();
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, frag).addToBackStack(null).commit();
         }
     }
 
@@ -61,23 +69,31 @@ public class EnterStudentChoicesActivity extends FragmentActivity {
         bt.startAnimation(fadeout);
     }
 
-    public void clickStudentProgramButton(View view) {
-        setFragmentView("languagetest");
+    private void setCurrentFragment(String currentFragment){
+        this.currentFragment = currentFragment;
     }
 
-    public void clickTOEFLButton(View view) {
-        setFragmentView("toefl");
+    private String getCurrentFragment(){
+        return this.currentFragment;
     }
 
-    public void clickIELTSButton(View view) {
-        setFragmentView("ielts");
+    private String getNextFragment(){
+        if (getCurrentFragment() == "program")
+            nextFragment = "languagetest";
+        else if(getCurrentFragment() == "languagetest")
+            nextFragment = "ielts";
+        else if(getCurrentFragment() == "toefl")
+            nextFragment = "gpa";
+        else if(getCurrentFragment() == "ielts")
+            nextFragment = "gpa";
+        else if(getCurrentFragment() == "gpa")
+            nextFragment = "gpacalculator";
+        return this.nextFragment;
     }
 
-    public void clickTOEFLProceedButton(View view) {
-        setFragmentView("gpa");
-    }
-
-    public void clickIELTSProceedButton(View view) {
-        setFragmentView("gpa");
+    public void clickProceedButton(View view){
+        Log.d("asdads ",getCurrentFragment()+" "+getNextFragment());
+        setFragmentView(getNextFragment());
+        Log.d("asdads ", getCurrentFragment() + " " + getNextFragment());
     }
 }
