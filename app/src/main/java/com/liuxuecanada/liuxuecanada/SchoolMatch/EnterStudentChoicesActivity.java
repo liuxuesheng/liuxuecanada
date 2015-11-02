b3bd47a604cf35002ddf5614b0a9d873b0532330
Licong Tang <litang@blackberry.com>
2015-11-02 13:17:50 -0500

    remove fragments pages

--- app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/SchoolMatch/EnterStudentChoicesActivity.java
@@ -2,9 +2,7 @@ package com.liuxuecanada.liuxuecanada.SchoolMatch;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -13,9 +11,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.liuxuecanada.liuxuecanada.R;
 import com.liuxuecanada.liuxuecanada.Utils.AsyncResponse;
@@ -23,70 +18,24 @@ import com.liuxuecanada.liuxuecanada.Utils.BlurDrawable;
 import com.liuxuecanada.liuxuecanada.Utils.ComponentsInViewService;
 import com.liuxuecanada.liuxuecanada.Utils.PaintService;
 import com.liuxuecanada.liuxuecanada.Utils.ServerResponse;
-import com.liuxuecanada.liuxuecanada.Utils.WheelView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class EnterStudentChoicesActivity extends FragmentActivity
-        implements FragmentIELTS.OnSeekBarUpdateListener,
-        FragmentTop.OnProgressCirclePageUpdateListener,
-        FragmentAcdemicType.OnFragmentCreatedListener,
-        FragmentProgram.OnFragmentCreatedListener,
-        FragmentLanguageTest.OnFragmentCreatedListener,
+        implements
         ViewTreeObserver.OnGlobalLayoutListener,
         AsyncResponse {
 
-    private static final String[] PLANETS = new String[]{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Pluto"};
     private final String mainURL = "http://10.135.51.51/liuxuecanadaserver/tests/test1/index.php?page=";
     LinearLayout layout = null;
     LinkedList<JSONArray> pagell = null;
-    JSONArray jsonArray3 = null;
-    JSONObject jObj = null;
     JSONArray arr = null;
-    private LinkedList<String> allFlowItemNames = null;
-    private Fragment fragTutorial = null;
-    private Fragment frag = null;
-    private String currentFragment = null;
-    private Button proceedButton = null;
-    private TextView topText = null;
-    private boolean disabledButton = true;
 
-    private static String convertStreamToString(InputStream is) {
-    /*
-     * To convert the InputStream to String we use the BufferedReader.readLine()
-     * method. We iterate until the BufferedReader return null which means
-     * there's no more data to read. Each line will appended to a StringBuilder
-     * and returned as String.
-     */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
-
+    private HashMap<String, String> userSelection = new HashMap<String, String>();
 
     @Override
     public void onTaskComplete(Object out) {
@@ -122,10 +71,7 @@ public class EnterStudentChoicesActivity extends FragmentActivity
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getActionBar().hide();
 
-        allFlowItemNames = new LinkedList<>();
-
         setContentView(R.layout.flow_main);
-
 
         findViewById(R.id.fragment_top_container).setBackgroundColor(Color.rgb(30, 136, 229));
         findViewById(R.id.fragment_container).setBackgroundColor(Color.rgb(238, 238, 238));
@@ -135,34 +81,7 @@ public class EnterStudentChoicesActivity extends FragmentActivity
 
         pud.execute(mainURL + 1);
 
-
-/*        if ((findViewById(R.id.fragment_container) != null) && (findViewById(R.id.fragment_top_container) != null) && (findViewById(R.id.fragment_bottom_container) != null)) {
-            frag = new FragmentAcdemicType();
-            fragTop = new FragmentTop();
-            fragBottom = new FragmentBottom();
-            frag.setArguments(getIntent().getExtras());
-            fragTop.setArguments(getIntent().getExtras());
-            fragBottom.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_top_container, fragTop).addToBackStack(null).commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_bottom_container, fragBottom).addToBackStack(null).commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, frag).addToBackStack(null).commit();
-            setCurrentFragment("acdemicstudy");
-            allFlowItemNames.addLast("acdemicstudy");
-        }*/
-
     }
-
-    private WheelView createWheelView() {
-        WheelView wv = new WheelView(this);
-        wv.setItems(Arrays.asList(PLANETS));
-        wv.setOnWheelViewListener(new WheelView.OnWheelViewListener() {
-            @Override
-            public void onSelected(int selectedIndex, String item) {
-            }
-        });
-        return wv;
-    }
-
 
     public void onGlobalLayout() {
         //layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -179,10 +98,6 @@ public class EnterStudentChoicesActivity extends FragmentActivity
 
     }
 
-    public void resetTextColorCount() {
-        PaintService.setTextPainted(false);
-    }
-
     @Override
     public void onBackPressed() {
         if (pagell == null || pagell.size() <= 1) {
@@ -196,139 +111,10 @@ public class EnterStudentChoicesActivity extends FragmentActivity
         }
     }
 
-    public void updateProceedButton() {
-        proceedButton = (Button) findViewById(R.id.proceed_studentchoices_button);
-        proceedButton.setTextColor(getResources().getColor(R.color.Red500));
-        disabledButton = false;
-    }
-
-    public void updateTitleText(String fragmentName) {
-        String chineseName = null;
-        if (fragmentName == "acdemicstudy")
-            chineseName = "Choose Acdemic Study";
-        if (fragmentName == "program") {
-            chineseName = "Choose Acdemic Area";
-        } else if (fragmentName == "languagetest") {
-            chineseName = "Choose Language Test";
-        } else if (fragmentName == "toefl") {
-            chineseName = "Enter TOEFL Results";
-        } else if (fragmentName == "ielts") {
-            chineseName = "Enter IELTS Results";
-        } else if (fragmentName == "gpa") {
-            chineseName = "Enter your GPA";
-        } else if (fragmentName == "gpacalculator") {
-            chineseName = "Use GPA Calculator";
-        }
-        topText = (TextView) findViewById(R.id.topTextString);
-        topText.setText(chineseName);
-    }
-
-    public void updateProgressCircle() {
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressbarid);
-        progressBar.setProgress(60);
-        TextView textProgress = (TextView) findViewById(R.id.textView1);
-        textProgress.setText("3/5");
-    }
-
-    private void setFragmentView(String whichFragment, Boolean forward) {
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-
-        if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-/*            if (savedInstanceState != null) {
-                return;
-            }*/
-            if (whichFragment == "acdemicstudy")
-                frag = new FragmentAcdemicType();
-            else if (whichFragment == "program")
-                frag = new FragmentProgram();
-            else if (whichFragment == "languagetest")
-                frag = new FragmentLanguageTest();
-            else if (whichFragment == "toefl")
-                frag = new FragmentTOEFL();
-            else if (whichFragment == "ielts")
-                frag = new FragmentIELTS();
-            else if (whichFragment == "gpa")
-                frag = new FragmentGPA();
-            else if (whichFragment == "gpacalculator")
-                frag = new FragmentGPACalculator();
-
-            frag.setArguments(getIntent().getExtras());
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            if (forward)
-                transaction.setCustomAnimations(R.anim.enter, R.anim.exit);
-            else
-                transaction.setCustomAnimations(R.anim.popenter, R.anim.popexit);
-            transaction.replace(R.id.fragment_container, frag).addToBackStack(null).commit();
-
-            if (forward)
-                allFlowItemNames.addLast(whichFragment);
-            else
-                allFlowItemNames.removeLast();
-        }
-    }
-
-    private void addTutorialFragment() {
-        if (fragTutorial == null) {
-            fragTutorial = new FragmentTutorial();
-            fragTutorial.setArguments(getIntent().getExtras());
-        }
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_tutorial_container, fragTutorial).addToBackStack(null).commit();
-    }
-
-    private void removeTutorialFragment() {
-        Fragment fragment = (FragmentTutorial) getSupportFragmentManager().findFragmentById(R.id.fragment_tutorial_container);
-        if (fragment != null)
-            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
-        fragTutorial = null;
-    }
-
     private void animateFade(int id, boolean dofadeout) {
         Button bt = (Button) findViewById(id);
         Animation fadeout = AnimationUtils.loadAnimation(this, R.anim.exit);
         bt.startAnimation(fadeout);
-    }
-
-    private String getCurrentFragment() {
-        return this.currentFragment;
-    }
-
-    private void setCurrentFragment(String fragment) {
-        this.currentFragment = fragment;
-    }
-
-    private String getPreviousFragment(String currentFragment) {
-        int index = -1;
-        for (int i = 0; i < allFlowItemNames.size(); i++) {
-            if (allFlowItemNames.get(i).equals(currentFragment)) {
-                index = i;
-                break;
-            }
-        }
-
-        if (index == -1 || index == 0)
-            return null;
-        else
-            return allFlowItemNames.get(index - 1);
-    }
-
-    private String getNextFragment(String currentFragment) {
-        int index = -1;
-        for (int i = 0; i < allFlowItemNames.size(); i++) {
-            if (allFlowItemNames.get(i).equals(currentFragment)) {
-                index = i;
-                break;
-            }
-        }
-
-        if (index == -1 || index == allFlowItemNames.size() - 1)
-            return null;
-        else
-            return allFlowItemNames.get(index + 1);
     }
 
     private void setBlurBackground() {
@@ -336,35 +122,5 @@ public class EnterStudentChoicesActivity extends FragmentActivity
         View blurView = findViewById(R.id.fragment_tutorial_container);
         BlurDrawable blurDrawable = new BlurDrawable(beneathView, 40);
         blurView.setBackground(blurDrawable);
-    }
-
-    public void clickProceedButton(View view) {
-        if (disabledButton) {
-            if (fragTutorial == null) {
-                addTutorialFragment();
-                setBlurBackground();
-            }
-        } else {
-            proceedButton.setTextColor(getResources().getColor(R.color.white));
-            String fragment = getNextFragment(getCurrentFragment());
-            setFragmentView(fragment, true);
-            setCurrentFragment(fragment);
-            updateTitleText(fragment);
-            disabledButton = true;
-        }
-    }
-
-    public void clickTutorialButton(View view) {
-        View blurView = findViewById(R.id.fragment_tutorial_container);
-        blurView.setBackgroundResource(0);
-        removeTutorialFragment();
-    }
-
-    public void clickProgramButton(View view) {
-        updateProceedButton();
-    }
-
-    public void clickLanguageTestButton(View view) {
-        updateProceedButton();
     }
 }
