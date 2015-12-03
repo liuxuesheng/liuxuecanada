384f8f2a5a0d704949cb1f3775bdba6863b70f6f
Licong Tang <litang@blackberry.com>
2015-12-03 11:17:54 -0500

    Add image slider link to server

--- app/src/main/java/com/liuxuecanada/liuxuecanada/NewsImageFragment.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/NewsImageFragment.java
@@ -5,20 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.AdapterView;
+import android.widget.Toast;
 
-import com.liuxuecanada.liuxuecanada.CustomizedComponent.ListViewItemComponent.ContentItem;
-import com.liuxuecanada.liuxuecanada.CustomizedComponent.ListViewItemComponent.ListAdapter;
 import com.liuxuecanada.liuxuecanada.News.NewsDisplayActivity;
 import com.liuxuecanada.liuxuecanada.SchoolMatch.EnterStudentChoicesActivity;
 import com.liuxuecanada.liuxuecanada.Utils.AsyncResponse;
-import com.liuxuecanada.liuxuecanada.Utils.PaintService;
 import com.liuxuecanada.liuxuecanada.Utils.ServerResponse;
 
 import org.json.JSONArray;
@@ -29,19 +29,25 @@ import java.util.ArrayList;
 
 public class NewsImageFragment extends Fragment
         implements
-        AsyncResponse{
+        AsyncResponse,
+        View.OnTouchListener, Handler.Callback {
 
     Activity activity;
-    private WebView imageWebView = null;
-    private String id = null;
-    private String srcUrl = null;
     ArrayList<String> new_image_ids = null;
     ArrayList<String> imagsrc = null;
     JSONArray arr = null;
+    private WebView imageWebView = null;
+    private String id = null;
+    private String srcUrl = null;
 
-    public static NewsImageFragment newInstance(int pos)
-   {
-       Log.d("asd8sd7sd ", "pos "+pos);
+    private static final int CLICK_ON_WEBVIEW = 1;
+    private static final int CLICK_ON_URL = 2;
+
+    private final Handler handler = new Handler(this);
+
+
+    public static NewsImageFragment newInstance(int pos) {
+        Log.d("asd8sd7sd ", "pos " + pos);
         NewsImageFragment f = new NewsImageFragment();
         Bundle b = new Bundle();
         b.putInt("pos", pos);
@@ -82,27 +88,19 @@ public class NewsImageFragment extends Fragment
 
             //10.135.31.47
             imageWebView = (WebView) activity.findViewById(R.id.webview_news_slider);
-            String imgsrc = (getArguments().getInt("pos") == 1)?imagsrc.get(0):imagsrc.get(1);
+            String imgsrc = (getArguments().getInt("pos") == 1) ? imagsrc.get(0) : imagsrc.get(1);
 
-            Log.d("asd8sd7sd ", "src "+imagsrc.get(0)+" "+imagsrc.get(1));
+            Log.d("asd8sd7sd ", "src " + imagsrc.get(0) + " " + imagsrc.get(1));
 
             Point size = new Point();
             activity.getWindowManager().getDefaultDisplay().getSize(size);
             int width = size.x;
-            String data="<html><body ><img id=\"resizeImage\" src=\""+"http://10.135.30.40/liuxuecanadaserver/news/" + imgsrc +"\" width=\"100%\" style=\"display:block; margin:auto; \"/></body></html>";
+            String data = "<html><body ><img id=\"resizeImage\" src=\"" + "http://10.135.30.40/liuxuecanadaserver/news/" + imgsrc + "\" width=\"100%\" style=\"display:block; margin:auto; \"/></body></html>";
             //alt="" align="middle"
-            Log.d("sdasdasds ",""+data);
+            Log.d("sdasdasds ", "" + data);
             imageWebView.loadData(data, "text/html; charset=UTF-8", null);
 
-            imageWebView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Intent myIntent = null;
-                    myIntent = new Intent(activity, EnterStudentChoicesActivity.class);
-                    myIntent.putExtra("test1", EnterStudentChoicesActivity.class);
-                    activity.startActivity(myIntent);
-                }
-            });
+            imageWebView.setOnTouchListener(this);
 
         } catch (JSONException ex) {
             ex.printStackTrace();
@@ -117,5 +115,28 @@ public class NewsImageFragment extends Fragment
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         this.activity = activity;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Log.d("asd8sd7sd ", "AAA ");
+        if (v.getId() == R.id.webview_news_slider && event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.d("asd8sd7sd ", "BBB ");
+            handler.sendEmptyMessageDelayed(CLICK_ON_WEBVIEW, 500);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        Log.d("asd8sd7sd ", "A ");
+        if (msg.what == CLICK_ON_WEBVIEW) {
+            Intent myIntent = null;
+            myIntent = new Intent(activity, NewsDisplayActivity.class);
+            myIntent.putExtra("record", ""+getArguments().getInt("pos"));
+            activity.startActivity(myIntent);
+            return true;
+        }
+        return false;
     }
 }
