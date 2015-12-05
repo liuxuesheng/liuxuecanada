ea5d2d1ea611738a65ce889a4e4b229913a14d6f
Licong <licongdavis@hotmail.com>
2015-12-05 10:16:38 -0500

    Update news slider to use imageview instead of webview

--- app/src/main/java/com/liuxuecanada/liuxuecanada/NewsImageFragment.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/NewsImageFragment.java
@@ -4,45 +4,37 @@ package com.liuxuecanada.liuxuecanada;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.liuxuecanada.liuxuecanada.News.NewsDisplayActivity;
 import com.liuxuecanada.liuxuecanada.Utils.AsyncResponse;
+import com.liuxuecanada.liuxuecanada.Utils.LoadImageFromURL;
 import com.liuxuecanada.liuxuecanada.Utils.ServerResponse;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Calendar;
-
 public class NewsImageFragment extends Fragment
         implements
-        AsyncResponse,
-        View.OnTouchListener, Handler.Callback {
+        AsyncResponse {
 
-    static final int MAX_CLICK_DURATION = 200;
-    private static final int CLICK_ON_WEBVIEW = 1;
-    static long startClickTime = 0;
-    private final Handler handler = new Handler(this);
     Activity activity;
     JSONArray arr = null;
-    private String new_image_ids = null;
+    private String news_image_ids = null;
     private String imagsrc = null;
     private WebView imageWebView = null;
     private TextView imageTextView = null;
     private String news_title = null;
-    private String current_item_section = null;
+    private String news_item_section = null;
 
     public static NewsImageFragment newInstance(int pos) {
         NewsImageFragment f = new NewsImageFragment();
@@ -68,18 +60,40 @@ public class NewsImageFragment extends Fragment
             arr = new JSONArray((String) out);
             JSONObject item = arr.getJSONObject(0);
 
+            ImageView iv = (ImageView) getView().findViewById(R.id.imageview_news_slider);
             //Image slider
             try {
 
-                new_image_ids = item.getString("id");
+                news_image_ids = item.getString("id");
                 imagsrc = item.getString("news_imageURL");
                 news_title = item.getString("news_title");
-                current_item_section = item.getString("section");;
+                news_item_section = item.getString("section");
+                ;
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
 
-            imageWebView = (WebView) getView().findViewById(R.id.webview_news_slider);
+            LoadImageFromURL loadImage = new LoadImageFromURL();
+            loadImage.execute("http://192.168.0.12/liuxuecanadaserver/news/" + imagsrc, iv, false);
+
+            final String newsId = news_image_ids;
+            final String current_item_section = news_item_section;
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent myIntent = null;
+                    myIntent = new Intent(activity, NewsDisplayActivity.class);
+                    myIntent.putExtra("record", newsId);
+                    myIntent.putExtra("section", current_item_section);
+                    activity.startActivity(myIntent);
+                }
+            });
+
+            imageTextView = (TextView) getView().findViewById(R.id.textview_news_slider);
+            imageTextView.setText(news_title);
+            imageTextView.setBackgroundColor(Color.argb(225, 255, 255, 255));
+
+           /* imageWebView = (WebView) getView().findViewById(R.id.webview_news_slider);
             Point size = new Point();
             activity.getWindowManager().getDefaultDisplay().getSize(size);
             int width = size.x;
@@ -87,9 +101,7 @@ public class NewsImageFragment extends Fragment
             imageWebView.loadData(data, "text/html; charset=UTF-8", null);
             imageWebView.setOnTouchListener(this);
 
-            imageTextView = (TextView) getView().findViewById(R.id.textview_news_slider);
-            imageTextView.setText(news_title);
-            imageTextView.setBackgroundColor(Color.argb(125, 255, 255, 255));
+           ;*/
 
         } catch (JSONException ex) {
             ex.printStackTrace();
@@ -105,38 +117,4 @@ public class NewsImageFragment extends Fragment
         super.onAttach(activity);
         this.activity = activity;
     }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (v.getId() == R.id.webview_news_slider) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN: {
-                    startClickTime = Calendar.getInstance().getTimeInMillis();
-                    break;
-                }
-                case MotionEvent.ACTION_UP: {
-                    long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
-                    if (clickDuration < MAX_CLICK_DURATION) {
-                        //click event has occurred
-                        Intent myIntent = null;
-                        myIntent = new Intent(activity, NewsDisplayActivity.class);
-                        myIntent.putExtra("record", "" + new_image_ids);
-                        myIntent.putExtra("section", current_item_section);
-                        activity.startActivity(myIntent);
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public boolean handleMessage(Message msg) {
-        if (msg.what == CLICK_ON_WEBVIEW) {
-
-            return true;
-        }
-        return false;
-    }
-
 }
