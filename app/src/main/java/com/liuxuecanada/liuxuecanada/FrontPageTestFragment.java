f7c1e2bcfce444b2610746d0e23fd11a299913fa
Licong Tang <litang@blackberry.com>
2015-12-08 17:51:35 -0500

    Add Test fragment image view

--- app/src/main/java/com/liuxuecanada/liuxuecanada/FrontPageTestFragment.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/FrontPageTestFragment.java
@@ -7,18 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.liuxuecanada.liuxuecanada.SchoolMatch.ChoicesFeedbackActivity;
 import com.liuxuecanada.liuxuecanada.SchoolMatch.EnterStudentChoicesActivity;
 import com.liuxuecanada.liuxuecanada.Utils.AsyncResponse;
+import com.liuxuecanada.liuxuecanada.Utils.LoadImageFromURL;
+import com.liuxuecanada.liuxuecanada.Utils.ServerResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FrontPageTestFragment extends Fragment
         implements
         AsyncResponse {
 
+    LinearLayout ll = null;
     private Activity activity;
+    private JSONArray arr = null;
 
     public static FrontPageTestFragment newInstance(String text) {
         FrontPageTestFragment f = new FrontPageTestFragment();
@@ -31,18 +40,6 @@ public class FrontPageTestFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pager_test, container, false);
-
-        TextView tv = new TextView(activity);
-        tv.setText("测评开始");
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = null;
-                myIntent = new Intent(activity, EnterStudentChoicesActivity.class);
-                myIntent.putExtra("test1", EnterStudentChoicesActivity.class);
-                activity.startActivity(myIntent);
-            }
-        });
 
         TextView tv2 = new TextView(activity);
         tv2.setText("测评结果");
@@ -67,17 +64,68 @@ public class FrontPageTestFragment extends Fragment
             }
         });
 
-        LinearLayout ll = (LinearLayout) v.findViewById(R.id.activity_test);
-        ll.addView(tv);
+        ll = (LinearLayout) v.findViewById(R.id.activity_test);
         ll.addView(tv2);
         ll.addView(tv3);
+
+        ServerResponse pud = new ServerResponse(this);
+        pud.execute("http://10.135.31.47/liuxuecanadaserver/frontPage_Test/index.php");
 
         return v;
     }
 
     @Override
     public void onTaskComplete(Object out) {
+        try {
+            arr = new JSONArray((String) out);
 
+            JSONObject item = null;
+            TextView tv = new TextView(activity);
+            ImageView iv = new ImageView(activity);
+
+            int newsImageWidth = 500;
+            int newsImageHeight = 300;
+
+            LinearLayout testRow = new LinearLayout(activity);
+            testRow.setOrientation(LinearLayout.HORIZONTAL);
+
+            try {
+                item = arr.getJSONObject(0);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            try {
+                LoadImageFromURL loadImage = new LoadImageFromURL();
+                loadImage.execute("http://10.135.31.47/liuxuecanadaserver/frontPage_Test/" + item.getString("imageURL"), iv, true, newsImageWidth, newsImageHeight);
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(newsImageWidth, newsImageHeight);
+                layoutParams.setMargins(30, 3, 30, 3);
+                iv.setLayoutParams(layoutParams);
+
+                iv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent myIntent = null;
+                        myIntent = new Intent(activity, EnterStudentChoicesActivity.class);
+                        myIntent.putExtra("test1", EnterStudentChoicesActivity.class);
+                        activity.startActivity(myIntent);
+                    }
+                });
+
+                tv.setText(item.getString("name"));
+
+                testRow.addView(iv);
+                testRow.addView(tv);
+                ll.addView(testRow);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
