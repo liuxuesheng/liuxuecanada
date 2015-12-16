70a9cf13f3a8040f346f994e98e84e023909fb96
Licong <licongdavis@hotmail.com>
2015-12-15 21:08:02 -0500

    Add navigation icon and logic

--- app/src/main/java/com/liuxuecanada/liuxuecanada/MainActivity.java
+++ app/src/main/java/com/liuxuecanada/liuxuecanada/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -26,6 +28,9 @@ public class MainActivity extends FragmentActivity {
     private FrontPageAdapter mAdapter;
     private ViewPager mPager;
     private ViewPager newsPager = null;
+    private LinearLayout lastLayout = null;
+    private TextView lastTextView = null;
+    private ImageView lastImageView = null;
 
     public static int getScreenWidth() {
         return screenWidth;
@@ -76,25 +81,43 @@ public class MainActivity extends FragmentActivity {
         mPager.setCurrentItem(0);
         mPager.setOffscreenPageLimit(1);
 
+        //Linear layout
+        final LinearLayout ll = (LinearLayout) findViewById(R.id.goto_news_ll);
+        final LinearLayout ll1 = (LinearLayout) findViewById(R.id.goto_profile_ll);
+        final LinearLayout ll2 = (LinearLayout) findViewById(R.id.goto_suggestions_ll);
+        final LinearLayout ll3 = (LinearLayout) findViewById(R.id.goto_messages_ll);
+
+        // Image view
+        final ImageView imageButton = (ImageView) findViewById(R.id.goto_news_iv);
+        final ImageView imageButton1 = (ImageView) findViewById(R.id.goto_profile_iv);
+        final ImageView imageButton2 = (ImageView) findViewById(R.id.goto_suggestions_iv);
+        final ImageView imageButton3 = (ImageView) findViewById(R.id.goto_messages_iv);
+
         // Watch for text click
         final TextView textButton = (TextView) findViewById(R.id.goto_news);
         final TextView textButton1 = (TextView) findViewById(R.id.goto_profile);
         final TextView textButton2 = (TextView) findViewById(R.id.goto_suggestions);
         final TextView textButton3 = (TextView) findViewById(R.id.goto_messages);
-        textButton.setPadding(60, 50, 60, 50);
-        textButton1.setPadding(60, 50, 60, 50);
-        textButton2.setPadding(60, 50, 60, 50);
-        textButton3.setPadding(60, 50, 60, 50);
+
+        // Set last visited
+        this.lastImageView = imageButton;
+        this.lastLayout = ll;
+        this.lastTextView = textButton;
+
+        //Set initial color
+        ll.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+        imageButton.setImageResource(R.drawable.ic_home_white_24dp);
+        textButton.setTextColor(Color.WHITE);
+
+        textButton.setPadding(60, 10, 60, 50);
+        textButton1.setPadding(60, 10, 60, 50);
+        textButton2.setPadding(60, 10, 60, 50);
+        textButton3.setPadding(60, 10, 60, 50);
         textButton.setTextSize(14);
         textButton1.setTextSize(14);
         textButton2.setTextSize(14);
         textButton3.setTextSize(14);
 
-        textButton.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textButton1.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textButton2.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textButton3.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textButton.setTextColor(Color.BLACK);
         textButton1.setTextColor(Color.BLACK);
         textButton2.setTextColor(Color.BLACK);
         textButton3.setTextColor(Color.BLACK);
@@ -103,46 +126,66 @@ public class MainActivity extends FragmentActivity {
         textButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mPager.setCurrentItem(0);
-                textButton.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                ll.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                imageButton.setImageResource(R.drawable.ic_home_white_24dp);
                 textButton.setTextColor(Color.WHITE);
-                ResetTextView(textButton1, textButton2, textButton3);
+                ResetLayout();
+                ResetLast(ll, imageButton, textButton);
             }
         });
 
         textButton1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mPager.setCurrentItem(1);
-                textButton1.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                ll1.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                imageButton1.setImageResource(R.drawable.ic_account_box_white_24dp);
                 textButton1.setTextColor(Color.WHITE);
-                ResetTextView(textButton, textButton2, textButton3);
+                ResetLayout();
+                ResetLast(ll1, imageButton1, textButton1);
             }
         });
 
         textButton2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mPager.setCurrentItem(2);
-                textButton2.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                ll2.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                imageButton2.setImageResource(R.drawable.ic_filter_drama_white_24dp);
                 textButton2.setTextColor(Color.WHITE);
-                ResetTextView(textButton, textButton1, textButton3);
+                ResetLayout();
+                ResetLast(ll2, imageButton2, textButton2);
             }
         });
 
         textButton3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mPager.setCurrentItem(3);
-                textButton3.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                ll3.setBackground(new ColorDrawable(Color.rgb(30, 136, 229)));
+                imageButton3.setImageResource(R.drawable.ic_message_white_24dp);
                 textButton3.setTextColor(Color.WHITE);
-                ResetTextView(textButton, textButton1, textButton2);
+                ResetLayout();
+                ResetLast(ll3, imageButton3, textButton3);
             }
         });
     }
 
-    private void ResetTextView(TextView textView1, TextView textView2, TextView textView3) {
-        textView1.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textView2.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textView3.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
-        textView1.setTextColor(Color.BLACK);
-        textView2.setTextColor(Color.BLACK);
-        textView3.setTextColor(Color.BLACK);
+    private void ResetLayout() {
+        this.lastLayout.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
+        String lastString = this.lastTextView.getText().toString();
+        if (lastString.equals("信息导航"))
+            this.lastImageView.setImageResource(R.drawable.ic_home_black_24dp);
+        else if (lastString.equals("个人信息"))
+            this.lastImageView.setImageResource(R.drawable.ic_account_box_black_24dp);
+        else if (lastString.equals("推荐系统"))
+            this.lastImageView.setImageResource(R.drawable.ic_filter_drama_black_24dp);
+        else if (lastString.equals("互动消息"))
+            this.lastImageView.setImageResource(R.drawable.ic_message_black_24dp);
+        this.lastTextView.setTextColor(Color.BLACK);
     }
+
+    private void ResetLast(LinearLayout ll, ImageView iv, TextView tv) {
+        this.lastLayout = ll;
+        this.lastImageView = iv;
+        this.lastTextView = tv;
+    }
+
 }
